2a84353c96d2fe4e3656aa908243260c8c7f9b87
Manuel Ruiz <manuelruiz@MacBook-Air-de-Esteban.local>
2015-01-09 17:50:02 -0300

    Initial Commit

---
+++ TuEntrada/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TuEntrada
+//
+//  Created by Manuel Ruiz on 1/9/15.
+//  Copyright (c) 2015 Ruitzei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TuEntrada/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TuEntrada
+//
+//  Created by Manuel Ruiz on 1/9/15.
+//  Copyright (c) 2015 Ruitzei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
